3b1fb1ff9aa5dac1a84956e221274cf4f96533d0
dany <eduardo_capua@outlook.com>
2023-10-18 13:10:40 -0300

    [ADD] coneccion

--- elecciones2023.py
+++ elecciones2023.py
@@ -3,9 +3,10 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1_ZuAYVX1KMWZz5DnhmwhdaUC49o7bM7mpTmQkgFNBKg/edit#gid=0"
+url = "https://docs.google.com/spreadsheets/d/1_ZuAYVX1KMWZz5DnhmwhdaUC49o7bM7mpTmQkgFNBKg/edit?usp=sharing"
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+data = conn.read(spreadsheet=url, usecols=list(range(7)), worksheet="120401593", ttl=0.5)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 st.dataframe(data)
+
