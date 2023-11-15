5be01c34bba5bd095e975b619a92ac4c3986007d
dany <eduardo_capua@outlook.com>
2023-11-15 13:19:26 -0300

    ADD cambios

--- elecciones2023.py
+++ elecciones2023.py
@@ -1,109 +1,54 @@
 
 
 import streamlit as st
-
-import folium
-from folium import plugins
-import pandas as pd
-from streamlit_folium import st_folium
-import datetime  # para poder realizar el query
-import pathlib
-import geopandas as gpd
+from streamlit_option_menu import option_menu
 
 
+st.set_page_config(page_title="ELECCIONES 2023",
+                   layout="centered")
+st.sidebar.success("selcciona la pagina", icon="✅")
+
+st.markdown(" # ELECCIONES 2023 ")
 
 
-# ventana y titulos
-st.set_page_config(page_title="ELECCIONES GENERALES 2023",
-                   initial_sidebar_state="expanded", layout="centered")
-st.subheader(body="ELECCIONES GENERALES 2023", divider=True, anchor="50")
-
-
-@st.cache_data
-def load_data(url):
-    # lectura del archivo y carga de datos
-    geo_json_sgo = pathlib.Path() / "Santiago del Estero.geojson"
-    assert geo_json_sgo.exists()
-    geo_json_sgo = gpd.read_file(geo_json_sgo)
-    return geo_json_sgo
-
-
-geo_json_sgo = load_data("Santiago del Estero.geojson")
-
-
-@st.cache_data
-def load_data(url):
-    # lectura del archivo y carga de datos
-    geo_json_circuito = pathlib.Path() / "Circuitos.geojson"
-    assert geo_json_circuito.exists()
-    geo_json_circuito = gpd.read_file(geo_json_circuito)
-    return geo_json_circuito
-
-
-geo_json_circuito = load_data("Circuitos.geojson")
-
-
-@st.cache_data
-def load_data(url):
-    # lectura del archivo y carga de datos
-    geo_json_Dpto = pathlib.Path() / "Departamentos Politicos.geojson"
-    assert geo_json_Dpto.exists()
-    geo_json_Dpto = gpd.read_file(geo_json_Dpto)
-    return geo_json_Dpto
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 2
 
 
 
+def streamlit_menu(example=1):
+    if example == 1:
+        # 1. as sidebar menu
+        with st.sidebar:
+            selected = option_menu(
+                menu_title="Main Menu",  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+            )
+        return selected
 
+    if example == 2:
+        # 2. horizontal menu w/o custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["ESCUELAS HABILITADAS", "MESAS HABILITADAS", "% DE VOTANTES"],  # required
+            default_index=0,  # optional
+            orientation="horizontal",
+        )
+        return selected
 
-geo_json_Dpto = load_data("Departamentos Politicos.geojson")
+ 
 
+selected = streamlit_menu(example=EXAMPLE_NO)
 
+if selected == "ESCUELAS HABILITADAS":
+    st.title(f"{selected}")
+if selected == "MESAS HABILITADAS":
+    st.title(f"{selected}")
 
-#@st.cache_data 
-#def load_data(url):
-#lectura del archivo y carga de datos
-df = pd.read_excel("dpto politico.xlsx")
-df = df.sort_values(by=["id"],ascending=[True])
-  # return df
+    
 
-#df = load_data("dpto politico.xlsx")
-
-st.button("Rerun")
-
-
-
-
-# bins = list(data_DptoPolitico["MOSTRAR"].quantile([0, 0.25, 0.5, 0.75, 1]))
-
-df["PORCENTAJE"] = pd.to_numeric(
-    df["PORCENTAJE"], errors="coerce")
-
-bins = list(df["PORCENTAJE"].quantile([0, 0.25, 0.50, 0.75, 1]))
-
-
-#st.dataframe(data_DptoPolitico)
-
-with st.container():
-    st.title("MAPA INTERACTIVO")
-
-with st.container():
-    mapa_prueba = folium.Map(tiles=None)
-
-    folium.TileLayer(name="OpenStreetMap",
-                     tiles="OpenStreetMap").add_to(mapa_prueba)
-
-    #folium.GeoJson(data=geo_json_sgo, name="Prov. de Sgo. del Estero",
-    #               show=False).add_to(mapa_prueba)
-
-    folium.Choropleth(geo_data=geo_json_Dpto, data=df, columns=["id", "PORCENTAJE"], key_on="feature.properties.id_Dpto", name="Departamento Politico",
-                      fill_color="BuPu", fill_opacity=0.7, line_opacity=0.5, legend_name="porcentaje de electores (%)", reset=True, show=True).add_to(mapa_prueba)
-
-    folium.LayerControl().add_to(parent=mapa_prueba, name="Filtro")
-
-    plugins.Fullscreen(position="topright", title="Ampliar",
-                       title_cancel="Salir", force_separate_button=True).add_to(mapa_prueba)
-
-    st_data3 = st_folium(mapa_prueba, use_container_width=any,
-                         center=[-27.8014259, -64.2855237], zoom=7, height=700, width=700)
-
-st.dataframe(df)
+if selected == "% DE VOTANTES":
+    st.title(f"{selected}")
---
+++ pages/1_porcentaje de escuelas.py
@@ -0,0 +1,218 @@
+import streamlit as st
+import folium
+from folium import plugins
+import pandas as pd
+from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+import datetime  # para poder realizar el query
+import pathlib
+import geopandas as gpd
+from streamlit_option_menu import option_menu
+from st_aggrid import AgGrid, GridOptionsBuilder, ColumnsAutoSizeMode, JsCode
+from streamlit_gsheets import GSheetsConnection
+
+# ventana y titulos
+st.set_page_config(page_title="PORCENTAJE ESTABLECIMIENTOS ESCOLARES",
+                   initial_sidebar_state="expanded", layout="centered")
+st.markdown(" ## PORCENTAJE ESTABLECIMIENTOS ESCOLARES ")
+
+
+
+
+
+# conección con google sheets
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+data = conn.read(worksheet="escuelas (estado)",usecols=list(range(5)), ttl=1)
+
+
+
+@st.cache_data
+def load_data(url):
+    # lectura del archivo y carga de datos
+    geo_json_centroide = pathlib.Path() / "centroides_dpto.geojson"
+    assert geo_json_centroide .exists()
+    geo_json_centroide  = gpd.read_file(geo_json_centroide)
+    return geo_json_centroide 
+
+
+geo_json_centroide  = load_data("Santiago del Estero.geojson")
+
+
+@st.cache_data
+def load_data(url):
+    # lectura del archivo y carga de datos
+    geo_json_circuito = pathlib.Path() / "Circuitos.geojson"
+    assert geo_json_circuito.exists()
+    geo_json_circuito = gpd.read_file(geo_json_circuito)
+    return geo_json_circuito
+
+
+geo_json_circuito = load_data("Circuitos.geojson")
+
+
+@st.cache_data
+def load_data(url):
+    # lectura del archivo y carga de datos
+    geo_json_Dpto = pathlib.Path() / "Departamentos Politicos.geojson"
+    assert geo_json_Dpto.exists()
+    geo_json_Dpto = gpd.read_file(geo_json_Dpto)
+    return geo_json_Dpto
+
+
+
+
+
+geo_json_Dpto = load_data("Departamentos Politicos.geojson")
+
+
+
+#@st.cache_data 
+#def load_data(url):
+#lectura del archivo y carga de datos
+df = pd.read_excel("dpto politico.xlsx")
+df = df.sort_values(by=["id"],ascending=[True])
+  # return df
+
+#df = load_data("dpto politico.xlsx")
+
+st.button("Rerun")
+
+
+
+
+#MENU HORIZONTAL
+
+
+EXAMPLE_NO = 2
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+def streamlit_menu(example=1):
+    if example == 1:
+        # 1. as sidebar menu
+        with st.sidebar:
+            selected = option_menu(
+                menu_title="Main Menu",  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+            )
+        return selected
+
+    if example == 2:
+        # 2. horizontal menu w/o custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["ESCUELAS HABILITADAS", "MESAS HABILITADAS", "% DE VOTANTES"],  # required
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+        )
+        return selected
+
+ 
+
+selected = streamlit_menu(example=EXAMPLE_NO)
+
+if selected == "ESCUELAS HABILITADAS":
+    st.title(f"{selected}")
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.dataframe(data)
+    with col2:
+        st.area_chart(data)
+
+if selected == "MESAS HABILITADAS":
+    st.title(f"{selected}")
+
+    
+if selected == "% DE VOTANTES":
+    st.title(f"{selected}")
+        
+    df["PORCENTAJE"] = pd.to_numeric(
+    df["PORCENTAJE"], errors="coerce")
+
+    bins = list(df["PORCENTAJE"].quantile([0, 0.25, 0.50, 0.75, 1]))
+
+
+    with st.container():
+        
+        st.title("MAPA INTERACTIVO")
+        
+        mapa_prueba = folium.Map(tiles=None, zoom_control= False)
+
+        
+        choropleth=folium.Choropleth(
+                        geo_data=geo_json_Dpto,
+                        data=df,
+                        columns=["id", "PORCENTAJE"],
+                        key_on="feature.properties.id", name="Departamento Politico",
+                        fill_color="BuPu",
+                        fill_opacity=0.7,
+                        nan_fill_color= "black",
+                        line_opacity=0.5, legend_name="porcentaje de electores (%)",
+                        reset=True,
+                        show=True,
+                        
+                        )
+        choropleth.add_to(mapa_prueba)
+        
+        tooltip = folium.GeoJsonTooltip(
+        fields=["id", "name"],
+        aliases=["id:", "nombre:"],
+        localize=False,
+        sticky=True,
+        labels=True,
+        style="""
+            background-color: #00000000;
+            border: 2px solid black;
+            border-radius: 3px;
+            box-shadow: 3px;
+        """,
+        max_width=800,
+        )
+        
+        centroides = folium.GeoJson(data=geo_json_Dpto,
+                                    name="centroides",
+                                    zoom_on_click=True,
+                                    tooltip=tooltip,
+                                    )
+        centroides.add_to(mapa_prueba)
+            
+        
+        filtro=folium.LayerControl(position="topleft")
+        filtro.add_to(
+                    parent=mapa_prueba,
+                    name="Filtro",
+                    
+                    )
+
+        agrandar_mapa=plugins.Fullscreen(
+                        position="topleft",
+                        title="Ampliar",
+                        title_cancel="Salir",
+                        force_separate_button=True,
+                        )
+        agrandar_mapa.add_to(mapa_prueba)
+
+        
+        herramienta_busqueda=plugins.Search(layer=centroides, search_label="name", geom_type="Polygon",collapsed=True)
+        herramienta_busqueda.add_to(mapa_prueba)
+        
+        
+        
+        #mostrar el mapa
+        st_folium(
+                mapa_prueba,
+                use_container_width=any,
+                center=[-27.807300, -63.232700],
+                zoom=7,
+                )    
+## MENU HORIZONTAL
+
+
+
+
+
